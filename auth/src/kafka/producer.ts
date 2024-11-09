2446439c182c4619b77051c0214573e1a31c60d3
Abin Babu <abinbabuonline@gmail.com>
2024-11-09 09:36:48 +0530

    kafka config, producer

---
+++ auth/src/kafka/producer.ts
@@ -0,0 +1,22 @@
+import kafka from "./config";
+import { Partitioners } from "kafkajs";
+
+async function produce(topic: string, value: string | Buffer): Promise<void> {
+    try {
+        const producer = kafka.producer({createPartitioner : Partitioners.LegacyPartitioner}) 
+        //LegacyPartitioner, which decides how messages are partitioned across Kafka's topics.
+        await producer.connect()
+        await producer.send({
+            topic,
+            messages: [{
+                value
+            }]
+        })
+
+        await producer.disconnect()
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default produce
