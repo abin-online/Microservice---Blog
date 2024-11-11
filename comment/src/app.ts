cef4dbe9f458974af01d5e668fd6e173cb5dc510
Abin Babu <abinbabuonline@gmail.com>
2024-11-11 19:59:28 +0530

    comment app

--- comment/src/app.ts
+++ comment/src/app.ts
@@ -2,13 +2,18 @@ import express from "express"
 import router from "./routes/commentRoute"
 import dotenv from "dotenv"
 import dbConnection from "./config/dbConnection"
+import consume from "./kafka/consumer"
 
 const app = express()
 dotenv.config()
 dbConnection()
 app.use(express.json());
 app.use(router)
+consume()
 
+app.get('/', (req, res) => {
+    res.json("from comment"); // Send a JSON response
+});
 
 app.listen(3002 , ()=> {
     console.log(`Server running on http://localhost:3002/`);
