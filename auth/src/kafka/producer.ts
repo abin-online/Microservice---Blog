0d7ccfd80382e02a87919afbdd92b6879e16d0ea
Abin Babu <abinbabuonline@gmail.com>
2024-11-11 19:57:27 +0530

    kafka producer set up in auth service

--- auth/src/kafka/producer.ts
+++ auth/src/kafka/producer.ts
@@ -5,15 +5,19 @@ async function produce(topic: string, value: string | Buffer): Promise<void> {
     try {
         const producer = kafka.producer({createPartitioner : Partitioners.LegacyPartitioner}) 
         //LegacyPartitioner, which decides how messages are partitioned across Kafka's topics.
-        await producer.connect()
+        console.log("Connecting to Kafka producer...");
+        await producer.connect();
+        console.log("Producer connected successfully.");
+
+        console.log(`Sending message to topic: ${topic} => ${value}`);
         await producer.send({
             topic,
-            messages: [{
-                value
-            }]
-        })
+            messages: [{ value }],
+        });
+        console.log("Message sent successfully.");
 
-        await producer.disconnect()
+        await producer.disconnect();
+        console.log("Producer disconnected.");
     } catch (error) {
         console.log(error);
     }
